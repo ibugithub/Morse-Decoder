b38717d57a74acfab8ea55c8ae8209fe0a543570
ibugithub <outsideworkibrahim@gmail.com>
2023-07-26 16:57:27 +0600

    Fix some linter  issues

--- decoder.rb
+++ decoder.rb
@@ -1,28 +1,27 @@
-
-$morse_code = {
-  '.-' => 'A',    '-...' => 'B',  '-.-.' => 'C',
-  '-..' => 'D',   '.' => 'E',     '..-.' => 'F',
-  '--.' => 'G',   '....' => 'H',  '..' => 'I',
-  '.---' => 'J',  '-.-' => 'K',   '.-..' => 'L',
-  '--' => 'M',    '-.' => 'N',    '---' => 'O',
-  '.--.' => 'P',  '--.-' => 'Q',  '.-.' => 'R',
-  '...' => 'S',   '-' => 'T',     '..-' => 'U',
-  '...-' => 'V',  '.--' => 'W',   '-..-' => 'X',
-  '-.--' => 'Y',  '--..' => 'Z'
-}
+MORSE_CODE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C',
+  '-..' => 'D', '.' => 'E', '..-.' => 'F',
+  '--.' => 'G', '....' => 'H', '..' => 'I',
+  '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R',
+  '...' => 'S', '-' => 'T', '..-' => 'U',
+  '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z'
+}.freeze
 
 def decode(code)
-  decoded_word = ""
-  code.split("   ").each do |word|
-    word.split(" ").each do |character|
-      char = $morse_code[character]
-      decoded_word+=char
+  decoded_word = ''
+  code.split('   ').each do |word|
+    word.split.each do |character|
+      char = MORSE_CODE[character]
+      decoded_word += char
     end
-    decoded_word+= " "
+    decoded_word += ' '
   end
-  return decoded_word
+  decoded_word
 end
 
-ibuscode = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
-decoded_msg = decode(ibuscode);
+ibuscode = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_msg = decode(ibuscode)
 puts decoded_msg
